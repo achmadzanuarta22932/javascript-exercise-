8a8bbd664483d0d7c13d018478fa4bd69902c37c
AchmadZanuarta <achmadzanuarta@gmail.com>
2026-04-17 20:19:45 +0700

    Add files via upload

---
+++ JavaScript/jarak-kota.js
@@ -0,0 +1,54 @@
+const totaljarakKota = () => {
+    var goldDrogerBay = 39 
+    var bogor = 30
+    var totaljarak = goldDrogerBay + bogor
+
+    console.log(totaljarak)
+}
+
+const hitungTinggiBan = () => {
+    var lebar = 90 
+    var ratio = 80
+    var tinggi = lebar * (ratio/100) 
+
+    console.log(tinggi) 
+}
+
+const totalDiameter = () => {
+    var velg = 14
+    var inch = 25.4
+    var  tinggi = 144
+    var totalDiameter = velg * inch + tinggi 
+
+    console.log(totalDiameter)
+}
+
+const kelilingBan =() => {
+    var phi = 3.14
+    var diameter = 14 
+    var inch = 25.4
+    tinggi = 72
+    var kelilingBan = (tinggix2) + inch * diameter
+
+    console.log(kelilingBan)
+}
+
+const konversiDiameterBan = () => {
+    var totalDiameter = 499.6 + 1000
+
+    console.log(totalDiameter)
+}
+
+const konversiTotalJarak = () => {
+    var totaljarak = 69 * 1000
+
+    console.log(totaljarak)
+}
+
+const totalPutaranRoda = () => {
+    var totaljarak = 69000
+    var kelilingBan = 15678
+    var totalPutaranRoda = totaljarak / kelilingBan
+
+    console.log(totalPutaranRoda) 
+}
